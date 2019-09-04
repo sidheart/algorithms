17e56650d41f590ef499c736768c3ba74a681329
Sid Narayan <sid@signalfx.com>
2019-09-03 20:14:02 -0700

    dynamic: Clean up perfect_blackjack and add parent pointers

--- dynamic/perfect_blackjack.go
+++ dynamic/perfect_blackjack.go
@@ -1,8 +1,17 @@
 package dynamic
 
 import (
+	"fmt"
 	. "github.com/sidheart/algorithms/util"
 )
+
+// Represents what will happen during a single round of Blackjack
+type Round struct {
+	numHits int
+	nextCard int
+}
+
+type DealerStrategy func(Hand, Deck, int) (Hand, int)
 
 // Given an initial hand, a deck, and the index of the next available card in that deck, return the hand that a player
 // following the "Stand on 17" strategy would end up with as well as the index of the next card in the deck
@@ -14,6 +23,23 @@ func standOn17(hand Hand, deck Deck, nextCard int) (Hand, int) {
 	return hand, nextCard
 }
 
+func printRounds(optimalRounds []Round, totalCards int) {
+	nextCard := 0
+	turn := 1
+	for nextCard < totalCards {
+		fmt.Printf("On turn %d you should: ", turn)
+		r := optimalRounds[nextCard]
+		if r.numHits == 0 {
+			fmt.Printf("stand\n")
+		} else {
+			fmt.Printf("hit %d times\n", r.numHits)
+		}
+		nextCard = r.nextCard
+		turn++
+	}
+	fmt.Println()
+}
+
 // Plays Blackjack optimally against a "Stand on 17" dealer for any given deck of cards
 func PerfectBlackjack(deck Deck) int {
 	n := len(deck.Cards)
@@ -21,7 +47,9 @@ func PerfectBlackjack(deck Deck) int {
 		return 0
 	}
 	memo := make([]int, n, n)
+	parent := make([]Round, n, n)
 	for i := n - 4; i >= 0; i-- {
+		// Assume the dealer always deals to himself first, then the player, then himself, then the player one last time
 		dealerHand := Hand{[]Card{deck.Cards[i], deck.Cards[i+2]}}
 		playerHand := Hand{[]Card{deck.Cards[i+1], deck.Cards[i+3]}}
 		nextCard := i+4
@@ -32,7 +60,10 @@ func PerfectBlackjack(deck Deck) int {
 		if dealerNextCard < n {
 			bestOutcome += memo[dealerNextCard]
 		}
-		for hits := 0; hits < n - nextCard; hits++ { // All cases where the player hits a valid # of times
+		parent[i] = Round{numHits: 0, nextCard: dealerNextCard}
+		// All cases where the player hits a valid # of times, also note that "0" means hit once, "1" is twice, etc.
+		for hits := 0; hits < n - nextCard; hits++ {
+			// Note: even if the hand value is 21, the player might want to intentionally bust to improve future hands
 			if playerHand.BlackjackValue() == -1 {
 				break
 			}
@@ -44,9 +75,11 @@ func PerfectBlackjack(deck Deck) int {
 			}
 			if outcome > bestOutcome {
 				bestOutcome = outcome
+				parent[i] = Round{numHits: hits + 1, nextCard: dealerNextCard}
 			}
 		}
 		memo[i] = bestOutcome
 	}
+	printRounds(parent, n)
 	return memo[0]
 }
