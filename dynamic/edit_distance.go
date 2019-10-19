f7948ec957911c19f9887907c33ece266f7cf65b
Sid Narayan <sid@signalfx.com>
2019-10-19 10:57:44 -0700

    dynamic: Revise solution for edit distance

--- dynamic/edit_distance.go
+++ dynamic/edit_distance.go
@@ -1,97 +1,64 @@
 package dynamic
 
-import (
-	"github.com/sidheart/algorithms/util"
-	"math"
-)
-
-type Action int
-
-const (
-	NONE Action = iota
-	INSERT
-	DELETE
-	REPLACE
-)
-
-type solution struct {
-	cost int
-	action Action
-}
-
-func maxSolution() solution {
-	return solution{math.MaxInt64, NONE}
-}
-
-func min(data  ...solution) solution {
-	if len(data) == 0 {
-		return solution{}
-	}
-	min := data[0]
-	for _, num := range data {
-		if num.cost < min.cost {
-			min = num
-		}
-	}
-	return min
-}
-
-// The cost of converting a character a into the character b by performing the specified action
-type conversionCost func (a byte, b byte, action Action) int
-
-func simpleCost(a byte, b byte, action Action) int {
+func wReplace(a byte, b byte) int {
 	if a == b {
 		return 0
 	}
-	switch action {
-	case INSERT:
-		return 1
-	case DELETE:
-		return 1
-	case REPLACE:
-		return 1
-	case NONE:
-		return math.MaxInt64
-	default:
-		return math.MaxInt64
-	}
+	return 1
 }
 
-func EditDistance(x string, y string) int {
-	solution := make(map[util.Cell]solution, len(x) * len(y))
-	return editDistance(x, y, 0, 0, simpleCost, solution)
+func wInsert(a byte) int {
+	return 1
 }
 
-func editDistance(x string, y string, xIndex int, yIndex int, costFunc conversionCost, memo map[util.Cell]solution) int {
-	if xIndex < 0 || yIndex < 0 {
-		panic("Negative indices are not supported")
+func wDelete(a byte) int {
+	return 1
+}
+
+func localMin(integers ...int) (min int, index int) {
+	n := len(integers)
+	if n == 0 {
+		return min, -1
 	}
-	n := len(x)
-	m := len(y)
-	cell := util.Cell{xIndex, yIndex}
-	sol, ok := memo[cell]
-	if ok {
-		return sol.cost
+	min, index = integers[0], 0
+	for i := 1; i < n; i++ {
+		if integers[i] < min {
+			min, index = integers[i], i
+		}
 	}
-	if n - xIndex <= 0 && m - yIndex <= 0 {
-		memo[cell] = solution{0, NONE}
-		return memo[cell].cost
+	return
+}
+
+func EditDistance(s1 string, s2 string) int {
+	rows, cols := len(s1), len(s2)
+	memoTable := make([][]int, rows)
+	for i, _ := range memoTable {
+		memoTable[i] = make([]int, cols)
 	}
-	var a, b byte
-	if xIndex < len(x)  {
-		a = x[xIndex]
+	return editDistance(&s1, &s2, &memoTable, 0, 0)
+}
+
+func editDistance(x *string, y *string, memoTable *[][]int, i int, j int) (solution int) {
+	m, n := len(*x) - i, len(*y) - j
+	if m <= 0 && n <= 0 {
+		return
 	}
-	if yIndex < len(y) {
-		b = y[yIndex]
+	if m <= 0 {
+		solution = wInsert((*y)[j]) + editDistance(x, y, memoTable, i, j+1)
+		return
 	}
-	insertCost, deleteCost := maxSolution(), maxSolution()
-	if b != 0 {
-		insertCost = solution{editDistance(x, y, xIndex, yIndex+1, costFunc, memo) + costFunc(a, b, INSERT), INSERT}
+	if n <= 0 {
+		solution = wDelete((*x)[i]) + editDistance(x, y, memoTable, i+1, j)
+		return
 	}
-	if a != 0 {
-		deleteCost = solution{editDistance(x, y, xIndex+1, yIndex, costFunc, memo) + costFunc(a, b, DELETE), DELETE}
+	solution = (*memoTable)[i][j]
+	if solution != 0 {
+		return
 	}
-	replaceCost := solution{editDistance(x, y, xIndex+1, yIndex+1, costFunc, memo) + costFunc(a, b, REPLACE), REPLACE}
-	memo[cell] = min(insertCost, deleteCost, replaceCost)
-	return memo[cell].cost
+	deleteCost := wDelete((*x)[i]) + editDistance(x, y, memoTable, i+1, j)
+	insertCost := wInsert((*y)[j]) + editDistance(x, y, memoTable, i, j+1)
+	replaceCost := wReplace((*x)[i], (*y)[j]) + editDistance(x, y, memoTable, i+1, j+1)
+	solution, _ = localMin(deleteCost, insertCost, replaceCost)
+	(*memoTable)[i][j] = solution
+	return
 }
