28fa2fa9bb3c7b1392fbc81eae4a239be2e7df80
Sid Narayan <sid@signalfx.com>
2019-11-29 20:22:45 -0800

    ctic: Added a solution for 1.8 zero matrix

---
+++ ctic/zero_matrix_1.8.go
@@ -0,0 +1,49 @@
+package ctic
+
+// coord represents a coordinate in a given matrix
+type coord struct {
+	row, col int
+}
+
+// zeroCross sets every element of the cross centered at (row, col) to 0
+func zeroCross(matrix [][]int, row, col int) {
+	m, n := len(matrix), len(matrix[0])
+	for curCol := 0; curCol < n; curCol++ {
+		matrix[row][curCol] = 0
+	}
+	for curRow := 0; curRow < m; curRow++ {
+		matrix[curRow][col] = 0
+	}
+}
+
+// copyMatrix allocates a new matrix that is identical to m, and returns it
+func copyMatrix(m [][]int) [][]int {
+	var matrix [][]int
+	for _, row := range m {
+		newRow := make([]int, len(row))
+		copy(newRow, row)
+		matrix = append(matrix, newRow)
+	}
+	return matrix
+}
+
+// zeroMatrix returns a matrix such that for every 0 in the original matrix, the entire row and column of that 0 is set
+// to 0 in the new matrix
+func zeroMatrix(m [][]int) [][]int {
+	if m == nil || len(m) == 0 || len(m[0]) == 0 {
+		return m
+	}
+	matrix := copyMatrix(m)
+	var coordsToZero []coord
+	for row, _ := range matrix {
+		for col, _ := range matrix[0] {
+			if matrix[row][col] == 0 {
+				coordsToZero = append(coordsToZero, coord{row, col})
+			}
+		}
+	}
+	for _, coord := range coordsToZero {
+		zeroCross(matrix, coord.row, coord.col)
+	}
+	return matrix
+}
---
+++ ctic/zero_matrix_1.8_test.go
@@ -0,0 +1,65 @@
+package ctic
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testMatrixA = [][]int{
+	{1, 0, 1},
+	{0, 1, 1},
+	{1, 1, 1},
+}
+
+var resMatrixA = [][]int{
+	{0, 0, 0},
+	{0, 0, 0},
+	{0, 0, 1},
+}
+
+var testMatrixB = [][]int{
+	{1, 1, 1},
+	{1, 0, 1},
+	{1, 1, 1},
+}
+
+var resMatrixB = [][]int{
+	{1, 0, 1},
+	{0, 0, 0},
+	{1, 0, 1},
+}
+
+var testMatrixC = [][]int{
+	{1, 1, 0},
+}
+
+var resMatrixC = [][]int{
+	{0, 0, 0},
+}
+
+var testMatrixD = [][]int{
+	{1, 1, 1},
+	{1, 1, 1},
+}
+
+var resMatrixD = testMatrixD
+
+var testMatrixE = [][]int{
+	{1, 1, 1},
+	{1, 1, 1},
+	{1, 1, 0},
+}
+
+var resMatrixE = [][]int{
+	{1, 1, 0},
+	{1, 1, 0},
+	{0, 0, 0},
+}
+
+func TestZeroMatrix(t *testing.T) {
+	assert.Equal(t, resMatrixA, zeroMatrix(testMatrixA))
+	assert.Equal(t, resMatrixB, zeroMatrix(testMatrixB))
+	assert.Equal(t, resMatrixC, zeroMatrix(testMatrixC))
+	assert.Equal(t, resMatrixD, zeroMatrix(testMatrixD))
+	assert.Equal(t, resMatrixE, zeroMatrix(testMatrixE))
+}
