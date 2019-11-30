0ce65d073393f67657e3e4cb3a1c6a351cdc3c4b
Sid Narayan <sid@signalfx.com>
2019-11-29 19:08:39 -0800

    citc: Added solution for string compression

---
+++ citc/string_compression_1.6.go
@@ -0,0 +1,33 @@
+package citc
+
+import (
+	"strconv"
+	"strings"
+)
+
+// compress compresses a string using counts of repeated characters
+// if the compressed version of the string is not smaller than the original, this function will return the original string
+// e.g. "aaabcccccaaa" becomes "a3b1c5a3"
+func compress(s string) string {
+	if len(s) <= 2 { // The compressed string will not become smaller than the original
+		return s
+	}
+	var sb strings.Builder
+	prevChar := s[0]
+	count := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == prevChar {
+			count++
+		} else {
+			sb.WriteString(string(prevChar) + strconv.Itoa(count))
+			count = 1
+			prevChar = s[i]
+		}
+	}
+	sb.WriteString(string(prevChar) + strconv.Itoa(count))
+	ret := sb.String()
+	if len(ret) < len(s) {
+		return ret
+	}
+	return s
+}
---
+++ citc/string_compression_1.6_test.go
@@ -0,0 +1,19 @@
+package citc
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCompress(t *testing.T) {
+	s := "aaabcccccaaa"
+	assert.Equal(t, "a3b1c5a3", compress(s))
+	s = "aaabccccca"
+	assert.Equal(t, "a3b1c5a1", compress(s))
+	s = "abc"
+	assert.Equal(t, "abc", compress(s))
+	s = ""
+	assert.Equal(t, "", compress(s))
+	s = "aaaaaaaaaabbbbbbbbbbbbbbb"
+	assert.Equal(t, "a10b15", compress(s))
+}
