c968fc847be9e559b2556b6bb48c9908cf5657d7
Sid Narayan <sid@signalfx.com>
2019-11-29 19:56:13 -0800

    citc: Add solution for 1.7 rotate matrix

---
+++ citc/rotate_matrix_1.7.go
@@ -0,0 +1,49 @@
+package citc
+
+type Pixel struct {
+	r,g,b,a byte
+}
+
+type image [][]Pixel
+
+// Equal returns true if image i is equal to other
+// Two images are equal if the contain the same Pixels in the same order
+func (i image) Equal(other image) bool {
+	if (i == nil) != (other == nil) {
+		return false
+	}
+	if len(i) != len(other) {
+		return false
+	}
+	if len(i) == 0 {
+		return true
+	}
+	if len(i[0]) != len(other[0]) {
+		return false
+	}
+	for row, _ := range i {
+		for col, _ := range i[0] {
+			if i[row][col] != other[row][col] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// rotateCW rotates an image clockwise
+func rotateCW(im image) image {
+	if len(im) == 0 || len(im[0]) == 0 {
+		return nil
+	}
+	m, n := len(im), len(im[0])
+	var rotIm image
+	for col := 0; col < n; col++ {
+		var newRow []Pixel
+		for row := m - 1; row >= 0; row-- {
+			newRow = append(newRow, im[row][col])
+		}
+		rotIm = append(rotIm, newRow)
+	}
+	return rotIm
+}
---
+++ citc/rotate_matrix_1.7_test.go
@@ -0,0 +1,40 @@
+package citc
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var red = Pixel{1, 0, 0, 0}
+var green = Pixel{0, 1, 0, 0}
+var blue = Pixel{0, 0, 1, 0}
+var redder = Pixel{2, 0, 0, 0}
+var greener = Pixel{0, 2, 0, 0}
+var bluer = Pixel{0, 0, 2, 0}
+var reddest = Pixel{3, 0, 0, 0}
+var greenest = Pixel{0, 3, 0, 0}
+var bluest = Pixel{0, 0, 3, 0}
+
+var testImage = image{
+{red, green, blue},
+{redder, greener, bluer},
+{reddest, greenest, bluest},
+}
+
+var testImageCW = image{
+{reddest, redder, red},
+{greenest, greener, green},
+{bluest, bluer, blue},
+}
+
+
+func TestEqual(t *testing.T) {
+	assert.True(t, testImage.Equal(testImage))
+	assert.True(t, testImageCW.Equal(testImageCW))
+	assert.False(t, testImage.Equal(testImageCW))
+}
+
+func TestRotateCW(t *testing.T) {
+
+	assert.True(t, testImageCW.Equal(rotateCW(testImage)))
+}
